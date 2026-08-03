8fe34cec004dba223789d002095e8becc51ea42d
khaoxkongz <thanapat.makkawal@gmail.com>
2026-08-04 02:51:20 +0700

    Update generated files, add new content, and adjust styles
    - Updated the timestamp in the generated index.js file.
    - Modified a line in the content file 350-dark-continent-1.md for clarity.
    - Added a new content file 354.md, titled "Isolation [1]", featuring a narrative set in a fantasy world.
    - Changed the class name in contents.$slug.tsx to improve styling.
    - Increased the typeset size in styles.css from 24px to 28px for better readability.

--- apps/web/src/routes/contents.$slug.tsx
+++ apps/web/src/routes/contents.$slug.tsx
@@ -205,7 +205,7 @@ function RouteComponent() {
       </div>
 
       <div
-        className="select-none"
+        className=""
         onClick={() => setAreControlsVisible((isVisible) => !isVisible)}
       >
         <article className="typeset typeset-docs mx-auto max-w-[75ch] rounded-xl border border-[oklch(0.9816_0.0026_106.45/10%)] bg-[oklch(0.499_0.0031_106.51/25%)] p-[clamp(1rem,3vw,1.75rem)] text-[oklch(0.9816_0.0026_106.45)] transition-[border-color] hover:border-[oklch(0.9816_0.0026_106.45/25%)]">
